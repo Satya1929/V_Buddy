6b59b39e85b754a765a1e24cc3d9aaab0cb9ac15
Satya1929 <spswain2003.org@gmail.com>
2024-09-14 17:25:32 +0530

    changes made

--- my_streamlit.py
+++ my_streamlit.py
@@ -1,3 +1,5 @@
 # to try different UI 
 # then do multipage
-# then deploy it
+    #developer page to see uploaded database files
+    #user dashboard = main page + conversational UI
+# then deploy it (streamlit free deployement + amazon aws deployment )
