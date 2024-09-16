ee2b893406a22732b5c1d725d2b30c984c81ea13
Satya1929 <spswain2003.org@gmail.com>
2024-09-16 11:34:25 +0530

    to do added

--- my_streamlit.py
+++ my_streamlit.py
@@ -1,5 +1,7 @@
-# to try different UI 
+# fix this , multiple uploads , ans answer only after a single upload
 # then do multipage
     #developer page to see uploaded database files
     #user dashboard = main page + conversational UI
+                    #answer it from pdf only and not from internet , also if taken from internet mention that
+# to try different UI
 # then deploy it (streamlit free deployement + amazon aws deployment )
